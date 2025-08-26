e29b434fa61845e111e5c353cb2cf1b77ebbaac4
Ernesto-Sevilla <estudianteernestosevilla@gmail.com>
2025-08-26 09:33:33 -0600

    Avances y aplicacion de ajustes del proyecto

---
+++ app.js
@@ -0,0 +1,32 @@
+// Importar las dependencias necesarias
+import express from "express";
+import cors from "cors";
+
+// Importar las rutas
+import rutasCategoria from "./routes/categorias.routes.js";
+
+//Crear la aplicacion de Express
+const app = express();
+
+// Habilitar CORS para cualquier origen
+app.use(cors({
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type"],
+}));
+
+// Middleware para parsear el cuerpo de las solicitudes
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+//Rutas
+app.use("/api", rutasCategorias);
+
+//Manejo de rutas no encontradas
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: "La ruta que ha especificado no se encuentra registrada."
+    });
+});
+
+//Export la aplicacion
+export default app;
